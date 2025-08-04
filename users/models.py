1c2616bb7eb07a4a8bcb2bbaa6752ca5531ccf7b
vadimko4 <vad.informat@gmail.com>
2025-08-05 00:06:43 +0400

    update models - create User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True,
+                               help_text="Загрузите свой аватар")
+    phone = models.CharField(max_length=15, verbose_name="Телефон", blank=True, null=True,
+                             help_text="Введите номер телефона")
+    country = models.CharField(max_length=50, verbose_name="Страна", blank=True, null=True,
+                             help_text="Введите страну")
+
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
+    USERNAME_FIELD = "email" # меняем юзернейм на почту
+    REQUIRED_FIELDS = ['username',]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        super().save(*args, **kwargs)
