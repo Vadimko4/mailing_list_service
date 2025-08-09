732e26950e6f8f6876e6b761d0956848bf471651
vadimko4 <vad.informat@gmail.com>
2025-08-09 13:15:16 +0400

    add model recipient, to configure it admin view, apply migrations

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from mailer.models import Letter
+from mailer.models import Letter, Recipient
 
 
 @admin.register(Letter)  # Регистрируем модель
 class LetterAdmin(admin.ModelAdmin):
     # Настраиваем поля, которые будем выводить в админке
     list_display = ('id', 'subject',)
+
+
+@admin.register(Recipient)  # Регистрируем модель
+class RecipientAdmin(admin.ModelAdmin):
+    # Настраиваем поля, которые будем выводить в админке
+    list_display = ('id', 'email', 'fio','comment')
+    # По чему будем делать фильтрацию
+    list_filter = ('email', 'fio', 'owner')
+    # По чему у нас будет поиск
+    search_fields = ('email', 'fio', 'owner')
--- mailer/models.py
+++ mailer/models.py
@@ -17,3 +17,23 @@ class Letter(models.Model):
         # permissions = [
         #     ("can_unpublish_product", "can unpublish product"),
         # ]
+
+    def __str__(self):
+        return self.subject
+
+
+class Recipient(models.Model):
+    email = models.EmailField(blank=False, null=False, verbose_name="Email", help_text='Введите email')
+    fio = models.CharField(max_length=150, verbose_name="ФИО", blank=True, null=True,
+                             help_text="Введите ФИО")
+    comment = models.TextField(verbose_name='Комментарий', help_text='Введите комментарий',default='отсутствует')
+    owner = models.ForeignKey(User, verbose_name="Владелец", help_text="Укажите владельца получателя", blank=True,
+                              null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'Получатель'
+        verbose_name_plural = 'Получатели'
+        ordering = ['owner', 'email', 'fio']
+
+    def __str__(self):
+        return self.email
