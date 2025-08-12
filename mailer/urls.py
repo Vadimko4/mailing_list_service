f660c836d4154857dceaa30780ad8ef0f13ba0a2
vadimko4 <vad.informat@gmail.com>
2025-08-13 01:45:53 +0400

    add create letters and delete letters

--- mailer/urls.py
+++ mailer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from mailer.apps import MailerConfig
 from django.views.decorators.cache import cache_page
 
-from mailer.views import LetterListView
+from mailer.views import LetterListView, LetterCreateView, LetterDeleteView
 
 app_name = MailerConfig.name
 
@@ -13,8 +13,8 @@ urlpatterns = [
     # path('products/', ProductListView.as_view(), name='home'),
     # path('contacts/', ContactsView.as_view(), name='contacts'),
     # path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
-    # path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('letters/create/', LetterCreateView.as_view(), name='letter_create'),
     # path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    # path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('letters/<int:pk>/delete/', LetterDeleteView.as_view(), name='letter_delete'),
     # path('categories/<int:pk>/', cache_page(60)(CategoryDetailView.as_view()), name='category_detail'),
 ]
--- mailer/views.py
+++ mailer/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView
 
+from mailer.forms import LetterForm
 from mailer.models import Letter
 from mailer.services import get_owner_letters_from_cache
 
@@ -12,3 +14,23 @@ class LetterListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         owner_id = self.request.user.id  # Получение id текущего пользователя
         return get_owner_letters_from_cache(owner_id)
+
+
+class LetterCreateView(LoginRequiredMixin, CreateView):
+    model = Letter
+    form_class = LetterForm
+
+    def get_success_url(self):
+        return reverse_lazy('mailer:letter_list', kwargs={'pk': self.request.user.pk})
+
+    # Чтобы автоматически установить текущего пользователя как владельца
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class LetterDeleteView(LoginRequiredMixin, DeleteView):
+    model = Letter
+
+    def get_success_url(self):
+        return reverse_lazy('mailer:letter_list', kwargs={'pk': self.request.user.pk})
