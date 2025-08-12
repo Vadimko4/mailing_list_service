61944c3752c3dfe2abd319be642a85de56f131b7
vadimko4 <vad.informat@gmail.com>
2025-08-13 02:06:01 +0400

    add update letters

--- mailer/urls.py
+++ mailer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from mailer.apps import MailerConfig
 from django.views.decorators.cache import cache_page
 
-from mailer.views import LetterListView, LetterCreateView, LetterDeleteView
+from mailer.views import LetterListView, LetterCreateView, LetterDeleteView, LetterUpdateView
 
 app_name = MailerConfig.name
 
@@ -14,7 +14,7 @@ urlpatterns = [
     # path('contacts/', ContactsView.as_view(), name='contacts'),
     # path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('letters/create/', LetterCreateView.as_view(), name='letter_create'),
-    # path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('letters/<int:pk>/delete/', LetterDeleteView.as_view(), name='letter_delete'),
+    path('letters/update/<int:pk>/', LetterUpdateView.as_view(), name='letter_update'),
+    path('letters/delete/<int:pk>/', LetterDeleteView.as_view(), name='letter_delete'),
     # path('categories/<int:pk>/', cache_page(60)(CategoryDetailView.as_view()), name='category_detail'),
 ]
--- mailer/views.py
+++ mailer/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DeleteView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 
 from mailer.forms import LetterForm
 from mailer.models import Letter
@@ -27,6 +27,14 @@ class LetterCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+
+class LetterUpdateView(LoginRequiredMixin, UpdateView):
+    model = Letter
+    form_class = LetterForm
+
+    def get_success_url(self):
+        return reverse_lazy('mailer:letter_list', kwargs={'pk': self.request.user.pk})
 
 
 class LetterDeleteView(LoginRequiredMixin, DeleteView):
